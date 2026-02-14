6db2ef14501fc300fac13ea46512dfd8b8b21c3f
elmouravidos <elmouravidos@gmail.com>
2026-02-15 00:33:42 +0100

    fix indentation error

--- app/main.py
+++ app/main.py
@@ -1,14 +1,7 @@
 from fastapi import FastAPI
-from fbref.fbref_player_data import load_fbref_player_data
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-return {"status": "API is running"}
-
-@app.get("/players")
-def players():
-url = "https://fbref.com/en/comps/Big5/stats/players/Big-5-European-Leagues-Stats"
-df = load_fbref_player_data(url)
-return df.head(20).to_dict(orient="records")
+    return {"status": "API is running"}
