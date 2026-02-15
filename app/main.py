c80d4d6cef6169b5be224c87b4d76eb796edaa28
elmouravidos <elmouravidos@gmail.com>
2026-02-15 23:07:03 +0100

    fix api players endpoint

--- app/main.py
+++ app/main.py
@@ -3,28 +3,24 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "*"
-]
+origins = ["*"]
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+CORSMiddleware,
+allow_origins=origins,
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"],
 )
-
 
 @app.get("/")
 def home():
-    return {"status": "API is running"}
-
+return {"status": "API is running"}
 
 @app.get("/players")
 def get_players():
-    return [
-        {"name": "Lionel Messi", "team": "Inter Miami"},
-        {"name": "Cristiano Ronaldo", "team": "Al Nassr"},
-        {"name": "Kylian Mbappe", "team": "Real Madrid"}
-    ]
+return [
+{"name": "Lionel Messi", "team": "Inter Miami"},
+{"name": "Cristiano Ronaldo", "team": "Al Nassr"},
+{"name": "Kylian Mbappe", "team": "Real Madrid"}
+]
