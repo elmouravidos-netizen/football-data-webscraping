0d1eef854f6dcf30a74e31fc38ba7590b80c82c4
elmouravidos <elmouravidos@gmail.com>
2026-02-15 12:29:49 +0100

    add players endpoint

--- app/main.py
+++ app/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"status": "API is running"}
+
+@app.get("/players")
+def players():
+return [
+{"name": "Lionel Messi", "club": "Inter Miami"},
+{"name": "Cristiano Ronaldo", "club": "Al Nassr"},
+{"name": "Kylian Mbappe", "club": "PSG"}
+]
